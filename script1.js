9c9b25a18eabb02b1d18bd4f6f5dc80998bd76fc
Alexey Basharovskii <alexey_baa@mail.ru>
2021-12-11 21:11:31 +0300

    про функции продолжение

--- script1.js
+++ script1.js
@@ -20,3 +20,25 @@ let drawCats = function (howManyTimes) {
 };
 
 drawCats(5);
+
+let printMultipleTimes = function (howManyTimes, whatToDraw) {
+  for (let i = 0; i < howManyTimes; i++) {
+    console.log(i + " " + whatToDraw);
+  }
+};
+
+printMultipleTimes(3, "(>_<)");
+
+console.log(Math.floor(145.45)); //округляет вниз до целого
+
+function double(number) {
+  return number * 2; // возврат значения функции
+}
+
+console.log(double(3));
+
+console.log(double(5) + double(6));
+
+console.log(double(5) + double(6));
+
+console.log(double(double(3)));
