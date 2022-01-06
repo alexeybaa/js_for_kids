e1c22c768581e644b526c27a003196036fbe4742
Alexey Basharovskii <alexey_baa@mail.ru>
2022-01-06 16:41:19 +0300

    тмена отложенного выполнения кода

--- script2.js
+++ script2.js
@@ -6,4 +6,14 @@ function timeUp() {
   alert("Время вышло!");
 }
 
-setTimeout(timeUp, 3000);
+setTimeout(timeUp, 3000); // миллисекунды
+
+// отмена отложенного выполнения кода
+function doHomeworkAlarm() {
+  alert("Эй! Пора делать домашку!");
+}
+
+let timeoutId = setTimeout(doHomeworkAlarm, 6000);
+clearTimeout(timeoutId); // clearTimeout отменил отложенный код
+
+// Многократный запуск кода и setInterval
