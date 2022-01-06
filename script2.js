8562f848c61ad13f5aa22e6d5f9575455e0fd018
Alexey Basharovskii <alexey_baa@mail.ru>
2022-01-06 19:06:56 +0300

    // Многократный запуск кода и setInterval

--- script2.js
+++ script2.js
@@ -14,6 +14,17 @@ function doHomeworkAlarm() {
 }
 
 let timeoutId = setTimeout(doHomeworkAlarm, 6000);
-clearTimeout(timeoutId); // clearTimeout отменил отложенный код
+//clearTimeout(timeoutId); // clearTimeout отменил отложенный код
 
 // Многократный запуск кода и setInterval
+let counter = 1;
+function printMessage() {
+  if (counter < 10) {
+    console.log("Ты смотришь в консоль уже " + counter + " сек");
+  }
+  counter++;
+}
+
+let intervalId = setInterval(printMessage, 1000);
+
+//clearInterval(intervalId); // clearInterval отменил отложенный код
